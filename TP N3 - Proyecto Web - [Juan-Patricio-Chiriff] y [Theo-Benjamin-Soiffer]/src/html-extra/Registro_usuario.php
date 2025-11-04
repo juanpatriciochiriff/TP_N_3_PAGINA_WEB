3cdba7e30d90aa4a3e88d3c95ad1768d296b76a6
juanpatriciochiriff <juanpatriciochiriff@gmail.com>
2025-11-04 17:48:21 -0300

    Add files via upload

---
+++ TP N3 - Proyecto Web - [Juan-Patricio-Chiriff] y [Theo-Benjamin-Soiffer]/src/html-extra/Registro_usuario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro 343</title>
+    <link rel="stylesheet" href="./css/registro.css">
+</head>
+<body>
+    <div class="container">
+        <div class="formulario">
+            <h2>Registro de Usuario</h2>
+            <?= $message ?>
+            <form method="POST" action="">
+                <label for="nombre_usuario">Nombre de usuario:</label>
+                <input type="text" id="nombre_usuario" name="nombre_usuario" required>
+                <br>
+                <label for="contrasena_usuario">Contraseña:</label>
+                <input type="password" id="contrasena_usuario" name="contrasena_usuario" required>
+                <br>
+                <label for="codigo_postal_usuario">Código postal:</label>
+                <input type="text" id="codigo_postal_usuario" name="codigo_postal_usuario" required>
+                <br>
+                <button type="submit" id="boton">Registrarse</button>
+            </form>
+            <p>¿Ya tenés cuenta? <a href="Inicio-sesionl.php">Iniciar sesión</a></p>
+        </div>
+        <div class="imagen">
+            <img src="./img/logo343.jpeg" alt="Logo 343">
+        </div>
+    </div>
+</body>
+</html>
