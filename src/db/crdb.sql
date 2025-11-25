ac26a8d1c2ede9c9e2a12ec1ee6f65ca74b21582
juanpatriciochiriff <juanpatriciochiriff@gmail.com>
2025-11-24 22:53:53 -0300

    Add files via upload

--- src/db/crdb.sql
+++ src/db/crdb.sql
@@ -1,12 +1,71 @@
-CREATE DATABASE IF NOT EXISTS crdb;
-USE crdb;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-11-2025 a las 02:53:20
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
-CREATE TABLE usuario (
-  id_usuario INT(11) NOT NULL AUTO_INCREMENT,
-  nombre_usuario VARCHAR(255) NOT NULL,
-  usuario_email VARCHAR(255) UNIQUE,
-  codigo_postal_usuario INT(11),
-  contrasena_usuario VARCHAR(255) NOT NULL,
-  PRIMARY KEY (id_usuario)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `crdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(255) NOT NULL,
+  `usuario_email` varchar(255) DEFAULT NULL,
+  `codigo_postal_usuario` int(11) DEFAULT NULL,
+  `contrasena_usuario` varchar(255) NOT NULL,
+  `suscrito` tinyint(1) NOT NULL DEFAULT 0,
+  `tipo_suscripcion` enum('mensual','anual') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nombre_usuario`, `usuario_email`, `codigo_postal_usuario`, `contrasena_usuario`, `suscrito`, `tipo_suscripcion`) VALUES
+(1, 'juan', NULL, 1234, '$2y$10$Xf7ZreJG.5PHDKAJhIDLUufxr2dcylD8aIrCpEst3UQsxXR9/pni.', 0, NULL);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD UNIQUE KEY `usuario_email` (`usuario_email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
